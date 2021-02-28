5edaeba3a28aa60a4f86b90f3197af8591fe8786
tomoyane <tomoyane111@gmail.com>
2021-02-28 20:58:59 +0900

    fix validation bug

--- validation.go
+++ validation.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -18,40 +19,55 @@ var allowedHttpMethod = []string{
 	http.MethodDelete,
 }
 
-func ValidateEnv() map[string]string {
-	result := make(map[string]string)
-	validateTargetUrl(result)
-	validateHttpMethods(result)
-	validateHttpHeaders(result)
-	validateReqHttpMethodPercentage(result)
-	validateHttpRequestBody(result)
-	validateThreadNum(result)
-	validateTrialNum(result)
-	validatePermanent(result)
-	validateSlackNotifyThreshHoldLatencyMillis(result)
-	return result
+func ValidateEnv() []error {
+	var errs []error
+	if err := validateTargetUrl(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateReqHttpMethodPercentage(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateHttpMethods(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateHttpHeaders(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateHttpRequestBody(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateThreadNum(); err != nil {
+		errs = append(errs, err)
+	}
+	if err:= validateTrialNum(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validatePermanent(); err != nil {
+		errs = append(errs, err)
+	}
+	if err := validateSlackNotifyThreshHoldLatencyMillis(); err != nil {
+		errs = append(errs, err)
+	}
+	return errs
 }
 
 // Validate TARGET_URL env
-func validateTargetUrl(result map[string]string) map[string]string {
+func validateTargetUrl() error {
 	env := os.Getenv(EnvTargetUrl)
 	if validateEmpty(env) {
-		result[EnvTargetUrl] = fmt.Sprintf("Environment valiable %s is required.", EnvTargetUrl)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvTargetUrl))
 	}
 	if !strings.HasPrefix(env, "http") || !strings.HasPrefix(env, "https") {
-		result[EnvTargetUrl] = fmt.Sprintf("Environment valiable %s has only http or https protocol.", EnvTargetUrl)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s has only http or https protocol.", EnvTargetUrl))
 	}
 	return nil
 }
 
 // Validate HTTP_METHODS env
-func validateHttpMethods(result map[string]string) map[string]string {
+func validateHttpMethods() error {
 	env := os.Getenv(EnvHttpMethods)
 	if validateEmpty(env) {
-		result[EnvHttpMethods] = fmt.Sprintf("Environment valiable %s is required.", EnvHttpMethods)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvHttpMethods))
 	}
 	isContain := false
 	methods := strings.Split(env, ",")
@@ -64,40 +80,35 @@ func validateHttpMethods(result map[string]string) map[string]string {
 		}
 	}
 	if !isContain {
-		result[EnvHttpMethods] = fmt.Sprintf("Environment valiable %s is only supprt %v.", EnvHttpMethods, allowedHttpMethod)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is only supprt %v.", EnvHttpMethods, allowedHttpMethod))
 	}
 	return nil
 }
 
 // Validate HTTP_HEADERS env
-func validateHttpHeaders(result map[string]string) map[string]string {
+func validateHttpHeaders() error {
 	env := os.Getenv(EnvHttpHeaders)
 	if validateEmpty(env) {
-		result[EnvHttpHeaders] = fmt.Sprintf("Environment valiable %s is required.", EnvHttpHeaders)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvHttpHeaders))
 	}
 	headers := make(map[string]interface{})
 	if err := json.Unmarshal([]byte(env), &headers); err != nil {
-		result[EnvHttpHeaders] = fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvHttpHeaders)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvHttpHeaders))
 	}
 	return nil
 }
 
 // Validate REQ_HTTP_METHOD_PERCENTAGES env
-func validateReqHttpMethodPercentage(result map[string]string) map[string]string {
+func validateReqHttpMethodPercentage() error {
 	methods := strings.Split(os.Getenv(EnvHttpMethods), ",")
-	if len(methods) > 1 {
+	if len(methods) > 0 {
 		env := os.Getenv(EnvReqHttpMethodPercentages)
 		if validateEmpty(env) {
-			result[EnvReqHttpMethodPercentages] = fmt.Sprintf("Environment valiable %s is required because method is multiple.", EnvReqHttpMethodPercentages)
-			return result
+			return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvReqHttpMethodPercentages))
 		}
 		percentages := make(map[string]int)
 		if err := json.Unmarshal([]byte(env), &percentages); err != nil {
-			result[EnvReqHttpMethodPercentages] = fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvReqHttpMethodPercentages)
-			return result
+			return errors.New(fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvReqHttpMethodPercentages))
 		}
 
 		var totalPercent int
@@ -105,78 +116,71 @@ func validateReqHttpMethodPercentage(result map[string]string) map[string]string
 			totalPercent = totalPercent + v
 		}
 		if totalPercent != 10 {
-			result[EnvReqHttpMethodPercentages] = fmt.Sprintf("Environment valiable %s requires percentage of 10.", EnvReqHttpMethodPercentages)
-			return result
+			return errors.New(fmt.Sprintf("Environment valiable %s requires percentage of 10.", EnvReqHttpMethodPercentages))
 		}
 	}
 	return nil
 }
 
 // Validate HTTP_REQ_BODY env
-func validateHttpRequestBody(result map[string]string) map[string]string {
+func validateHttpRequestBody() error {
 	env := os.Getenv(EnvHttpRequestBody)
 	if validateEmpty(env) {
-		result[EnvHttpRequestBody] = fmt.Sprintf("Environment valiable %s is required.", EnvHttpRequestBody)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvHttpRequestBody))
 	}
 	body := make(map[string]interface{})
 	if err := json.Unmarshal([]byte(env), &body); err != nil {
-		result[EnvHttpRequestBody] = fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvHttpRequestBody)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s not hashmap structure.", EnvHttpRequestBody))
 	}
 	return nil
 }
 
 // Validate THREAD_NUM env
-func validateThreadNum(result map[string]string) map[string]string {
+func validateThreadNum() error {
 	env := os.Getenv(EnvThreadNum)
 	if validateEmpty(env) {
-		result[EnvThreadNum] = fmt.Sprintf("Environment valiable %s is required.", EnvThreadNum)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvThreadNum))
 	}
 	if _, err := strconv.Atoi(env); err != nil {
-		result[EnvThreadNum] = fmt.Sprintf("Environment valiable %s is not number.", EnvThreadNum)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is not number.", EnvThreadNum))
 	}
 	return nil
 }
 
 // Validate TRIAL_NUM env
-func validateTrialNum(result map[string]string) map[string]string {
+func validateTrialNum() error {
 	env := os.Getenv(EnvTrialNum)
 	if validateEmpty(env) {
-		result[EnvTrialNum] = fmt.Sprintf("Environment valiable %s is required.", EnvTrialNum)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is required.", EnvTrialNum))
 	}
 	if _, err := strconv.Atoi(env); err != nil {
-		result[EnvTrialNum] = fmt.Sprintf("Environment valiable %s is not number.", EnvTrialNum)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is not number.", EnvTrialNum))
 	}
 	return nil
 }
 
 // Validate PERMANENT env
-func validatePermanent(result map[string]string) map[string]string {
+// Optional
+func validatePermanent() error {
 	env := os.Getenv(EnvPermanent)
 	if validateEmpty(env) {
-		return result
+		return nil
 	}
 	if !strings.EqualFold(env, "true") || !strings.EqualFold(env, "false") {
-		result[EnvPermanent] = fmt.Sprintf("Environment valiable %s is true or false.", EnvPermanent)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is true or false.", EnvPermanent))
 	}
 	return nil
 }
 
 // Validate SLACK_NOTIFY_THRESHOLD_LATENCY_MILLIS env
-func validateSlackNotifyThreshHoldLatencyMillis(result map[string]string) map[string]string {
+// Optional
+func validateSlackNotifyThreshHoldLatencyMillis() error {
 	env := os.Getenv(EnvSlackNotifyThreshHoldLatencyMillis)
 	if validateEmpty(env) {
-		return result
+		return nil
 	}
 	if _, err := strconv.Atoi(env); err != nil {
-		result[EnvSlackNotifyThreshHoldLatencyMillis] = fmt.Sprintf("Environment valiable %s is not number.", EnvSlackNotifyThreshHoldLatencyMillis)
-		return result
+		return errors.New(fmt.Sprintf("Environment valiable %s is not number.", EnvSlackNotifyThreshHoldLatencyMillis))
 	}
 	return nil
 }
--- validation_test.go
+++ validation_test.go
@@ -5,15 +5,30 @@ import (
 	"testing"
 )
 
+func TestValidateEnv(t *testing.T) {
+	os.Setenv(EnvSlackNotifyThreshHoldLatencyMillis, "invalid")
+	os.Setenv(EnvTargetUrl, "")
+	os.Setenv(EnvHttpMethods, "")
+	os.Setenv(EnvHttpHeaders, "")
+	os.Setenv(EnvReqHttpMethodPercentages, "")
+	os.Setenv(EnvHttpRequestBody, "")
+	os.Setenv(EnvThreadNum, "")
+	os.Setenv(EnvTrialNum, "")
+	result := ValidateEnv()
+	if result == nil {
+		t.Fatalf("Expect required validation error.")
+	}
+}
+
 func TestValidateTargetUrl(t *testing.T) {
 	os.Setenv(EnvTargetUrl, "")
-	result := validateTargetUrl(map[string]string{})
+	result := validateTargetUrl()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvTargetUrl)
 	}
 
 	os.Setenv(EnvTargetUrl, "invalid_url")
-	result = validateTargetUrl(map[string]string{})
+	result = validateTargetUrl()
 	if result == nil {
 		t.Fatalf("Expect prefix validation error. %s ", EnvTargetUrl)
 	}
@@ -21,13 +36,13 @@ func TestValidateTargetUrl(t *testing.T) {
 
 func TestValidateHttpMethods(t *testing.T) {
 	os.Setenv(EnvHttpMethods, "")
-	result := validateHttpMethods(map[string]string{})
+	result := validateHttpMethods()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvHttpMethods)
 	}
 
 	os.Setenv(EnvHttpMethods, "OPTION")
-	result = validateHttpMethods(map[string]string{})
+	result = validateHttpMethods()
 	if result == nil {
 		t.Fatalf("Expect not allowed validation error. %s", EnvHttpMethods)
 	}
@@ -35,13 +50,13 @@ func TestValidateHttpMethods(t *testing.T) {
 
 func TestValidateHttpHeader(t *testing.T) {
 	os.Setenv(EnvHttpHeaders, "")
-	result := validateHttpHeaders(map[string]string{})
+	result := validateHttpHeaders()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvHttpHeaders)
 	}
 
 	os.Setenv(EnvHttpHeaders, "Not map data")
-	result = validateHttpHeaders(map[string]string{})
+	result = validateHttpHeaders()
 	if result == nil {
 		t.Fatalf("Expect map parser validation error. %s", EnvHttpHeaders)
 	}
@@ -50,19 +65,19 @@ func TestValidateHttpHeader(t *testing.T) {
 func TestValidateReqHttpMethodPercentage(t *testing.T) {
 	os.Setenv(EnvHttpMethods, "GET,PUT")
 	os.Setenv(EnvReqHttpMethodPercentages, "")
-	result := validateReqHttpMethodPercentage(map[string]string{})
+	result := validateReqHttpMethodPercentage()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvReqHttpMethodPercentages)
 	}
 
 	os.Setenv(EnvReqHttpMethodPercentages, "Not map data")
-	result = validateReqHttpMethodPercentage(map[string]string{})
+	result = validateReqHttpMethodPercentage()
 	if result == nil {
 		t.Fatalf("Expect map parser validation error. %s", EnvReqHttpMethodPercentages)
 	}
 
 	os.Setenv(EnvReqHttpMethodPercentages, `{"GET": 7, "PUT":4}`)
-	result = validateReqHttpMethodPercentage(map[string]string{})
+	result = validateReqHttpMethodPercentage()
 	if result == nil {
 		t.Fatalf("Expect not filled percentage validation error. Just 10 percentage. %s", EnvReqHttpMethodPercentages)
 	}
@@ -70,13 +85,13 @@ func TestValidateReqHttpMethodPercentage(t *testing.T) {
 
 func TestValidateHttpRequestBody(t *testing.T) {
 	os.Setenv(EnvHttpRequestBody, "")
-	result := validateHttpRequestBody(map[string]string{})
+	result := validateHttpRequestBody()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvHttpRequestBody)
 	}
 
 	os.Setenv(EnvHttpRequestBody, "Not map data")
-	result = validateHttpRequestBody(map[string]string{})
+	result = validateHttpRequestBody()
 	if result == nil {
 		t.Fatalf("Expect map parser validation error. %s", EnvHttpRequestBody)
 	}
@@ -84,13 +99,13 @@ func TestValidateHttpRequestBody(t *testing.T) {
 
 func TestValidateThreadNum(t *testing.T) {
 	os.Setenv(EnvThreadNum, "")
-	result := validateThreadNum(map[string]string{})
+	result := validateThreadNum()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvThreadNum)
 	}
 
 	os.Setenv(EnvThreadNum, "string")
-	result = validateThreadNum(map[string]string{})
+	result = validateThreadNum()
 	if result == nil {
 		t.Fatalf("Expect number validation error. %s", EnvThreadNum)
 	}
@@ -98,41 +113,41 @@ func TestValidateThreadNum(t *testing.T) {
 
 func TestValidateTrialNum(t *testing.T) {
 	os.Setenv(EnvTrialNum, "")
-	result := validateTrialNum(map[string]string{})
+	result := validateTrialNum()
 	if result == nil {
 		t.Fatalf("Expect empty validation error. %s", EnvTrialNum)
 	}
 
 	os.Setenv(EnvTrialNum, "string")
-	result = validateTrialNum(map[string]string{})
+	result = validateTrialNum()
 	if result == nil {
 		t.Fatalf("Expect number validation error. %s", EnvTrialNum)
 	}
 }
 
 func TestValidatePermanent(t *testing.T) {
-	os.Setenv(EnvPermanent, "")
-	result := validatePermanent(map[string]string{})
+	os.Setenv(EnvPermanent, "invalid")
+	result := validatePermanent()
 	if result == nil {
-		t.Fatalf("Expect empty validation error. %s", EnvPermanent)
+		t.Fatalf("Expect invalid validation error. %s", EnvPermanent)
 	}
 
 	os.Setenv(EnvPermanent, "invalid")
-	result = validatePermanent(map[string]string{})
+	result = validatePermanent()
 	if result == nil {
 		t.Fatalf("Expect allowed true or false validation error. %s", EnvPermanent)
 	}
 }
 
 func TestValidateSlackNotifyThreshHoldLatencyMillis(t *testing.T) {
-	os.Setenv(EnvSlackNotifyThreshHoldLatencyMillis, "")
-	result := validateSlackNotifyThreshHoldLatencyMillis(map[string]string{})
+	os.Setenv(EnvSlackNotifyThreshHoldLatencyMillis, "invalid")
+	result := validateSlackNotifyThreshHoldLatencyMillis()
 	if result == nil {
-		t.Fatalf("Expect empty validation error. %s", EnvSlackNotifyThreshHoldLatencyMillis)
+		t.Fatalf("Expect invalid validation error. %s", EnvSlackNotifyThreshHoldLatencyMillis)
 	}
 
 	os.Setenv(EnvSlackNotifyThreshHoldLatencyMillis, "string")
-	result = validateSlackNotifyThreshHoldLatencyMillis(map[string]string{})
+	result = validateSlackNotifyThreshHoldLatencyMillis()
 	if result == nil {
 		t.Fatalf("Expect number validation error. %s", EnvSlackNotifyThreshHoldLatencyMillis)
 	}
