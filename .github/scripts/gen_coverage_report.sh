e513c08423f153d5ce25c41afc2e3c1a1c05c8ac
tomoyane <tomoyane111@gmail.com>
2021-02-28 11:38:09 +0900

    rename coverage report

--- .github/scripts/gen_coverage_report.sh
+++ .github/scripts/gen_coverage_report.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-go test -race -covermode atomic -coverprofile=cover.out ./... && go tool cover -html=cover.out -o cover.html
+go test -race -covermode atomic -coverprofile=cover.out ./... && go tool cover -html=cover.out -o index.html
 
 git config --global user.name tomoyane
 git config --global user.email "${UPPY_DRIVER_EMAIL}"
@@ -11,7 +11,7 @@ git remote -v
 
 git fetch
 git checkout coverage
-git add --force cover.html
+git add --force index.html
 git commit -m "[ci skip] Update coverage"
 git remote set-url --push origin https://tomoyane:${GITHUB_TOKEN}@github.com/tomoyane/http-continuous-benchmarking.git
 git push origin HEAD:coverage --force
