f75ddd1dc6014da70ccc0fc60f49fe3a6402ded6
tomoyane <tomoyane111@gmail.com>
2021-03-02 19:07:36 +0900

    fix linux command

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,7 +5,7 @@ set -eu
 GOOS=linux GOARCH=amd64 go build
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 
-now=$(date "+%Y%m%d%H%M%S")date "+%Y%m%d%H%M%S"
+now=$(date "+%Y%m%d%H%M%S")
 docker build -t tomohito/http-continuous-benckmarking:"${now}" .
 docker push tomohito/http-continuous-benckmarking:"${now}"
 
